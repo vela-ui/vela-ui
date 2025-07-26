f18c1227362cdf430e38a4c03e912170576fd97c
Bobby <8290615@qq.com>
2025-07-26 17:37:48 +0800

    feat: add combobox component

---
+++ packages/react/src/icons/chevrons-up-down.tsx
@@ -0,0 +1,19 @@
+export const ChevronsUpDownIcon = (props: React.ComponentProps<"svg">) => {
+  return (
+    <svg
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="m7 15 5 5 5-5" />
+      <path d="m7 9 5-5 5 5" />
+    </svg>
+  )
+}
